502103eabc16182925fc7e664483a497c3ecea7f
zhouziyuan <zhouziyuan@xingfeiinc.com>
2021-07-28 18:52:24 +0800

    1

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     # 广东移动App签到
     cmccValue = jsonValue['CMCC']
     cmcc = CMCC(sessionid, cmccValue)
-    # cmcc.runAction()
-    # weixin_send_msg(' '.join(cmcc.weixin), openid)
-    # save_readme(cmcc.weixin)
+    cmcc.runAction()
+    weixin_send_msg(' '.join(cmcc.weixin), openid)
+    save_readme(cmcc.weixin)
 
     # 比思签到+赚取每日金币
     hkpicValue = jsonValue['HKPIC']
