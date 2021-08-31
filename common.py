784f0324f1d925a5ec736ec44e57af73bdebc43e
zhouziyuan <zhouziyuan@xingfeiinc.com>
2021-08-31 14:39:23 +0800

    1

--- common.py
+++ common.py
@@ -330,7 +330,9 @@ def print_error(message):
 
 
 def print_success(message):
-    print('\033[7;30;32m{message}\033[0m'.format(message=message))
+    global DEBUG
+    if DEBUG:
+        print('\033[7;30;32m{message}\033[0m'.format(message=message))
 
 
 # ----------------下面是移动签到所使用到的方法，已废弃-----------------------------------------------------------------------------------------------
